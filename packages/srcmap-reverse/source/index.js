1d358b3dd0f8014bb4142ea9b306083790422627
Will Johnson <william.c.johnson@intel.com>
2017-07-07 17:01:43 -0400

    - Get rid of server file - Bring code from server back into index and eliminate the function - Export the server const

--- packages/srcmap-reverse/source/index.js
+++ packages/srcmap-reverse/source/index.js
@@ -1,2 +1,35 @@
-import server from './server.js';
-export default server();
+// @flow
+
+//
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+import http from 'http';
+import highland from 'highland';
+import reverseInParallel from './reverse-in-parallel.js';
+import clientErrorsLog from './logger.js';
+
+import type { HighlandStreamT } from 'highland';
+
+const server = http.createServer(
+  (request: http.IncomingMessage, response: http.ServerResponse) => {
+    const r: HighlandStreamT<Buffer> = highland(request);
+    r
+      .map(x => x.toString('utf-8'))
+      .collect()
+      .map((x: string[]) => x.join(''))
+      .flatMap((x: string) => {
+        const { trace }: { trace: string } = JSON.parse(x);
+        clientErrorsLog.error({ trace }, 'Client Error Trace');
+        return reverseInParallel(trace);
+      })
+      .map(xs => JSON.stringify(xs))
+      .pipe(response);
+  }
+);
+
+const port: number = +process.env.npm_package_config_port;
+server.listen(port);
+
+export default server;
