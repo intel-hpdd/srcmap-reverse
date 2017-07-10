95f447ac7297824432b3ba8f185790f0b9ccb502
Will Johnson <william.c.johnson@intel.com>
2017-07-10 14:02:33 -0400

    GH-28 pull GUI as dep of srcmap-reverser + bundle it's map Fixes #28
    As we move to make this a stand-alone service, we can't rely on external
    modules supplying a critical file. Make GUI a dev dependency of this
    module. Then, when bundling, move the GUI srcmap into the dist directory
    + have this module reference that location.
    
    - set NODE_ENV to "test" when running tests
    - remove references to srcMapFile as we no longer need to use a config
      variable
    - No need to include mock-env.js in srcmap-reverser jest config
    - include @iml/gui as a dev dependency in srcmap-reverser and copy the
      sourcemap to the srcemap-reverser/dist directory
    - hardcode the sourcemap name and load the file based on the NODE_ENV
      environment.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- packages/srcmap-reverser/jest.config.js
+++ packages/srcmap-reverser/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   resetModules: true,
   coveragePathIgnorePatterns: ['/node_modules/', 'test', 'dist'],
-  transformIgnorePatterns: ['/node_modules/(?!@iml)/'],
-  setupTestFrameworkScriptFile: './test/mock-env.js'
+  transformIgnorePatterns: ['/node_modules/(?!@iml)/']
 };
