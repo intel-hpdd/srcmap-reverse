690ca1bb2279319c5caf0aa21203ddc24edb266d
Will Johnson <william.c1.johnson@gmail.com>
2017-07-25 10:59:24 -0400

    GH-52 Remove GUI as a dependency (#53)
    * GH-52 Remove GUI as a dependency
    Fixes #52.
    
    We've decided to package GUI into its own RPM. This means we no longer
    need to include GUI as a dependency in srcmapreverse.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>
    
    * - Update version to 3.0.4

--- test/reverser.test.js
+++ test/reverser.test.js
@@ -8,9 +8,9 @@ const reversedLine =
   'at /Users/wkseymou/projects/chroma/chroma-manager/chroma_ui_new/source/chroma_ui/iml/dashboard/dashboard-filter-controller.js:161:14';
 
 describe('reverser', () => {
-  let mockSrcMapReverse, mockFs, sourceMapStream, reverser, spy;
+  let mockSrcMapReverse, mockFs, sourceMapStream, reverser, mockGlob, spy;
 
-  beforeEach(done => {
+  beforeEach(() => {
     spy = jest.fn(() => 'spy');
     mockSrcMapReverse = jest.fn(() => reversedLine);
     jest.mock('../source/srcmap-reverse.js', () => mockSrcMapReverse);
@@ -24,33 +24,68 @@ describe('reverser', () => {
     };
     jest.mock('fs', () => mockFs);
 
-    reverser = require('../source/reverser.js').default;
+    mockGlob = {
+      sync: jest.fn(() => ['/usr/lib/iml-manager/iml-gui/main.fc123.js.map'])
+    };
+    jest.mock('glob', () => mockGlob);
 
-    reverser('test/fixtures/built-fd5ce21b.js.map.json')(
-      highland([
+    reverser = require('../source/reverser.js').default;
+  });
+
+  describe('with a sourcmap file specified', () => {
+    beforeEach(done => {
+      reverser('test/fixtures/built-fd5ce21b.js.map.json')(
+        highland([
+          'at Object.DashboardFilterCtrl.$scope.filter.onFilterView (https://localhost:8000/static/chroma_ui/built-fd5ce21b.js:38:7096)'
+        ])
+      ).each(x => {
+        spy(x);
+        done();
+      });
+    });
+
+    it('should call createReadStream', () => {
+      const sourceMapPath: string = mockFs.createReadStream.mock.calls[0][0];
+      expect(
+        sourceMapPath.indexOf('test/fixtures/built-fd5ce21b.js.map.json')
+      ).toBeGreaterThan(-1);
+    });
+
+    it('should call srcmapReverse', () => {
+      expect(mockSrcMapReverse).toHaveBeenCalledWith(
+        'source-maps',
         'at Object.DashboardFilterCtrl.$scope.filter.onFilterView (https://localhost:8000/static/chroma_ui/built-fd5ce21b.js:38:7096)'
-      ])
-    ).each(x => {
-      spy(x);
-      done();
+      );
+    });
+
+    it('should return the reversed line', () => {
+      expect(spy).toHaveBeenCalledWith(reversedLine);
     });
   });
 
-  it('should call createReadStream', () => {
-    const sourceMapPath: string = mockFs.createReadStream.mock.calls[0][0];
-    expect(
-      sourceMapPath.indexOf('test/fixtures/built-fd5ce21b.js.map.json')
-    ).toBeGreaterThan(-1);
-  });
+  describe('without a sourcemap file specified', () => {
+    beforeEach(done => {
+      reverser()(
+        highland([
+          'at Object.DashboardFilterCtrl.$scope.filter.onFilterView (https://localhost:8000/static/chroma_ui/built-fd5ce21b.js:38:7096)'
+        ])
+      ).each(x => {
+        spy(x);
+        done();
+      });
+    });
 
-  it('should call srcmapReverse', () => {
-    expect(mockSrcMapReverse).toHaveBeenCalledWith(
-      'source-maps',
-      'at Object.DashboardFilterCtrl.$scope.filter.onFilterView (https://localhost:8000/static/chroma_ui/built-fd5ce21b.js:38:7096)'
-    );
-  });
+    it('should call glob.sync', () => {
+      expect(mockGlob.sync).toHaveBeenCalledWith(
+        '/usr/lib/iml-manager/iml-gui/main.*.js.map'
+      );
+    });
 
-  it('should return the reversed line', () => {
-    expect(spy).toHaveBeenCalledWith(reversedLine);
+    it('should call createReadStream', () => {
+      const sourceMapPath: string = mockFs.createReadStream.mock.calls[0][0];
+      expect(
+        sourceMapPath.indexOf('/usr/lib/iml-manager/iml-gui/main.fc123.js.map')
+      ).toBeGreaterThan(-1);
+    });
   });
 });
